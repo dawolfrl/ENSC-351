00be05ef3cdf17af48f04f1a621e3d284ca4c8f8
Diane Wolf <dawolf@sfu.ca>
2018-09-25 19:04:33 -0700

    DW: Flush works, converted all functions to support struct format. Dereferencing null pointer problem.

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -1,5 +1,3 @@
-// TO DO
-// - implement flushing mechanism
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,25 +9,22 @@ const int DATA_MAX = 120; // maximum characters to store in line of trace
 struct traceStruct {
 	int position; // position of trace in buffer of 10 000
 	char type; // type of event
-	string name;
-	string cat;
+	char* name;
+	char* category;
 	char phase;
+	void* objectID;
 	long long timestamp;
 	int pid;
 	int tid;
 	char scope;
+	char* key;
+	char* value;
 };
 ofstream json_file; // json file name/path for tracing
 traceStruct traceBuffer[BUFFER_MAX]; // buffer for traces
 int bufferCounter = 0; // number of traceStructs in the buffer
 
 // function prototypes
-/*
-* @description Generates dummy events for testing.
-* @parameters [a, b] a > b, number of events generated is a + 1
-* @returns none
-*/
-int recursion(int a, int b);
 /*
 * @description Opens json file for tracing.
 * @parameters [filename] pointer to file name/path
@@ -88,6 +83,7 @@ void trace_end();
 int main() {
     //char path[] = "trace.json"; // file path for lab
 	char path[] = "C:\\Users\\D\\Desktop\\traceDummy.json"; // file path for DW's PC
+	// dummy variables for testing
 	char instant_name[] = "Instant"; // name for instant event
 	char name_array[] = "Test"; // name of all events
 	char cat_array[] = "Duration"; // category of all events
@@ -106,18 +102,34 @@ int main() {
 	char* counter_key_ptr = counter_key;
 	char* counter_value_ptr = counter_value;
 	
-	trace_start(filename_ptr); // begin output (opening square bracket)
-	int i = 0;
-	/*trace_event_start(test_name, test_cat);
-	trace_event_start(test_name, test_cat);
-	trace_event_start(test_name, test_cat);
-	trace_event_end();
-	trace_event_end();
-	trace_event_end();
-	trace_object_new(obj_name_ptr, obj_ptr); // generate object event
-	trace_counter(counter_name_ptr, counter_key_ptr, counter_value_ptr); // generate counter event
-	trace_object_gone(obj_name_ptr, obj_ptr); // end object event*/
-	trace_instant_global(instant_ptr); // generate instant event
+	// testing
+	int loop = 0;
+	while (loop != 20) {
+		trace_start(filename_ptr); // begin output (opening square bracket)
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_object_new(obj_name_ptr, obj_ptr); // generate object event
+		trace_object_gone(obj_name_ptr, obj_ptr); // end object event
+		trace_counter(counter_name_ptr, counter_key_ptr, counter_value_ptr); // generate counter event
+		trace_instant_global(instant_ptr); // generate instant event
+		trace_event_end();
+		trace_event_end();
+		trace_event_end();
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_start(test_name, test_cat);
+		trace_event_end();
+		trace_event_end();
+		trace_event_end();
+		trace_event_end();
+		trace_event_end();
+		trace_event_end();
+		loop++;
+	}
 	trace_flush();
 	trace_end(); // end output (closing square bracket)
 	return 0;
@@ -129,105 +141,112 @@ void trace_start(char* filename) {
 	json_file << "[\n"; // opening bracket for trace file
 }
 void trace_event_start(char* name, char* categories) {
-	json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << name << "\", "; // name
-	json_file << "\"cat\": \"" << categories << "\", "; // category
-	json_file << "\"ph\": \"B\", "; // phase
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1"; // thread ID
-	json_file << "}," << endl; // terminate line with closing brace and comma
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'S'; // S = start
+	// event info
+	traceBuffer[bufferCounter].name = name;
+	traceBuffer[bufferCounter].category = categories;
+	traceBuffer[bufferCounter].phase = 'B';
+	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
+	traceBuffer[bufferCounter].pid = 1;
+	traceBuffer[bufferCounter].tid = 1;
+	bufferCounter++;
 }
 void trace_event_end() {
-	json_file << "{"; // opening brace
-	json_file << "\"ph\": \"E\", "; // phase
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1"; // thread ID
-	json_file << "}"; // closing brace
-	json_file << ",";
-	json_file << endl; // terminate with newline
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'E'; // E = end
+	// event info
+	traceBuffer[bufferCounter].phase = 'E';
+	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
+	traceBuffer[bufferCounter].pid = 1;
+	traceBuffer[bufferCounter].tid = 1;
+	bufferCounter++;
 }
 void trace_instant_global(char* name) {
-	traceBuffer[bufferCounter].position = bufferCounter + 1;
-	traceBuffer[bufferCounter].type = 'i';
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'I'; // I = instant
+	// event info
 	traceBuffer[bufferCounter].name = name;
 	traceBuffer[bufferCounter].phase = 'i';
 	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
 	traceBuffer[bufferCounter].pid = 1;
 	traceBuffer[bufferCounter].tid = 1;
-	traceBuffer[bufferCounter].scope = 'g';
+	traceBuffer[bufferCounter].scope = 'g'; // g = global
 	bufferCounter++;
-	/*json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << name << "\", "; // name
-	json_file << "\"ph\": \"i\", "; // phase
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1, "; // thread ID
-	json_file << "\"s\": \"g\""; // scope
-	json_file << "}"; // closing brace
-	json_file << ",";
-	json_file << endl; // terminate with newline*/
 }
 void trace_object_new(char* name, void* obj_pointer){
-	json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << name << "\", "; // name
-	json_file << "\"ph\": \"N\", "; // phase
-	json_file << "\"id\": \"" << obj_pointer << "\", "; // object ID
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1"; // thread ID
-	json_file << "}," << endl; // terminate line with closing brace and comma
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'N'; // N = object new
+	// event info
+	traceBuffer[bufferCounter].name = name;
+	traceBuffer[bufferCounter].phase = 'N';
+	traceBuffer[bufferCounter].objectID = obj_pointer;
+	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
+	traceBuffer[bufferCounter].pid = 1;
+	traceBuffer[bufferCounter].tid = 1;
+	bufferCounter++;
 }
 void trace_object_gone(char* name, void* obj_pointer){
-	json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << name << "\", "; // name
-	json_file << "\"ph\": \"D\", "; // phase
-	json_file << "\"id\": \"" << obj_pointer << "\", "; // object ID
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1"; // thread ID
-	json_file << "}"; // closing brace
-	json_file << ",";
-	json_file << endl; // terminate with newline
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'G'; // G = object gone
+	// event info
+	traceBuffer[bufferCounter].name = name;
+	traceBuffer[bufferCounter].phase = 'D';
+	traceBuffer[bufferCounter].objectID = obj_pointer;
+	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
+	traceBuffer[bufferCounter].pid = 1;
+	traceBuffer[bufferCounter].tid = 1;
+	bufferCounter++;
 }
 void trace_counter(char* name, char* key, char* value){
-	json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << name << "\", "; // name
-	json_file << "\"ph\": \"C\", "; // phase
-	json_file << "\"ts\": "; // timestamp
-	json_file << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"args\": {\"" << key << "\": " << value << "}"; // arguments (counter)
-	json_file << "}"; // closing brace
-	json_file << ",";
-	json_file << endl; // terminate with newline
+	// internal info
+	traceBuffer[bufferCounter].position = bufferCounter + 1; // line number
+	traceBuffer[bufferCounter].type = 'C'; // C = counter
+	// event info
+	traceBuffer[bufferCounter].name = name;
+	traceBuffer[bufferCounter].phase = 'C';
+	traceBuffer[bufferCounter].timestamp = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count();
+	traceBuffer[bufferCounter].pid = 1;
+	traceBuffer[bufferCounter].tid = 1;
+	traceBuffer[bufferCounter].key = key; // argument
+	traceBuffer[bufferCounter].value = value; // argument
+	bufferCounter++;
 }
 void trace_flush() {
 	int i = 0;
-	json_file << "{"; // opening brace
-	json_file << "\"name\": \"" << traceBuffer[i].name << "\", "; // name
-	json_file << "\"ph\": \"i\", "; // phase
-	json_file << "\"ts\": "; // timestamp
-	json_file << traceBuffer[i].timestamp;
-	json_file << ", ";
-	json_file << "\"pid\": 1, "; // process ID
-	json_file << "\"tid\": 1, "; // thread ID
-	json_file << "\"s\": \"g\""; // scope
-	json_file << "}"; // closing brace
-	json_file << ",";
-	json_file << endl; // terminate with newline
+	while (i != bufferCounter) {
+		json_file << "{"; // opening brace
+		if (traceBuffer[i].type != 'E') {
+			json_file << "\"name\": \"" << traceBuffer[i].name << "\", "; // name
+			if (traceBuffer[i].type == 'S') {
+				json_file << "\"cat\": \"" << traceBuffer[i].category << "\", "; // category
+			}
+		}
+		json_file << "\"ph\": \"" << traceBuffer[i].phase << "\""; // phase
+		if (traceBuffer[i].type == 'N') {
+			json_file << ", \"id\": \"" << traceBuffer[i].objectID << "\""; // object ID
+		}
+		json_file << ", \"ts\": " << traceBuffer[i].timestamp; // timestamp
+		json_file << ", \"pid\": " << traceBuffer[i].pid; // process ID
+		if (traceBuffer[i].type == 'C') {
+			json_file << ", \"args\": {\"" << traceBuffer[i].key << "\": " << traceBuffer[i].value << "}"; // arguments (counter)
+		}
+		else {
+			json_file << ", \"tid\": " << traceBuffer[i].tid; // thread ID
+		}
+		if (traceBuffer[i].type == 'I') {
+			json_file << ", \"s\": \"" << traceBuffer[i].scope << "\""; // scope
+		}
+		json_file << "}," << endl; // closing brace and newline
+		i++;
+	}
+	memset(&traceBuffer, 0, bufferCounter);
+	bufferCounter = 0; // reset buffer
 }
 void trace_end() {
 	json_file << "{}]"; // extra curlies to offset comma problem, closing bracket for trace file
