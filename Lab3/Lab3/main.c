89d3ae7cd6007f1b2e286e434b3ce728f5f14854
dawolfrl <32075998+dawolfrl@users.noreply.github.com>
2018-10-11 17:52:16 -0700

    DW: Added file reading check, started tokenizing.

--- Lab3/Lab3/main.c
+++ Lab3/Lab3/main.c
@@ -1,26 +1,50 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
 
-void processInput(char *filename);
+void processInput(char *filename, char *tokenarrayptr);
 void map();
 void reduce();
 void print();
 
 int main() {
-	char data[] = "data.txt";
-	// array for strings?
+    printf("beginning main function...\n");
+
+	char data[] = "data.txt"; // file name
+	char wordArray[256];// array for tokenized text
+	char *wordArrayptr = &wordArray;
 	// array for key-value pairs?
 	// array for reduced key-value pairs?
-	processInput(data);
+	processInput(data,wordArrayptr);
 
+    printf("ending main function...\n");
 	return 0;
 }
 
-void processInput(char *filename) {
-	FILE *fileptr;
-	fileptr = fopen(filename,"r");
-	// read lines and separate words
-	fclose(fileptr);
+void processInput(char *filename, char *tokenarrayptr) {
+    printf("beginning processInput function...\n");
+
+    char filechar;
+	FILE *fileptr; // file type pointer
+	char line[256]; // array to store line from file
+	char token[50]; // array to store token
+    int tokenArrayIterator = 0;
+
+	fileptr = fopen(filename,"r"); // open file for reading
+    if(fileptr == NULL){
+        perror(">> Error opening file");
+        return(-1);
+    }
+
+	while(!feof(fileptr)){
+        fgets(line,20,fileptr);
+        token = strtok(line," ");
+        tokenarrayptr[tokenArrayIterator] = token;
+        tokenArrayIterator++;
+	}
+
+	fclose(fileptr); // close file
+	printf("ending processInput function...\n");
 }
 void map() {
 	// create key-value pair for each word
